d4369091704c191f2fd79cd06b97cf92b616854a
ViktorSmolin <54165337+ViktorSmolin@users.noreply.github.com>
2022-02-07 21:41:57 +0700

    Add files via upload

---
+++ webPack/src/index.js
@@ -0,0 +1,13 @@
+// index.js
+
+import example from './images/example.png';
+
+// создаем элемент заголовка
+const heading = document.createElement('h1')
+heading.textContent = 'Как интересно!';
+
+// добавляем заголовок в DOM
+const root = document.querySelector('#root')
+root.append(heading);
+
+
